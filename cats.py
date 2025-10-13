4358f2b964383442aac7131c9e2726c80be4d8ae
Regeace <playmarket_pochta@mail.ru>
2025-10-13 20:43:21 +0300

    Добавлена галерея с картинками /cats

--- cats.py
+++ cats.py
@@ -1,16 +1,25 @@
 from flask import Flask, render_template
+from utils import *
 
 app = Flask(__name__)
 
 
+def form_contex(data: dict):
+    context = data
+    return context
+
 @app.route('/index')
 def index():
+    """Страница для различных тестов."""
     return render_template(template_name_or_list='index.html')
 
 
 @app.route('/cats')
 def cats():
-    return render_template(template_name_or_list='cats_gallery.html')
+    """Базовая страница галереи с наполнением из различных баз данных."""
+    context = form_contex(CATS_DB)
+    leader_score = max(context.values())[0]
+    return render_template(template_name_or_list='cats_gallery.html', context=context, leader_score=leader_score)
 
 
 app.run(host='localhost', port=8000)
---
+++ utils.py
@@ -0,0 +1,12 @@
+CATS_DB = {
+    '1': [10, '/cats/1686656313_koshki-13.jpg'],
+    '2': [1, '/cats/1687456031_zhivotnye-9.jpg'],
+    '3': [15, '/cats/1687530058_kotiki-14.jpg'],
+    '4': [15, '/cats/1687254280_zhivotnye-1.jpg'],
+    '5': [1, '/cats/1687530059_kotiki-16.jpg'],
+    '6': [1, '/cats/1689607933_koshki-23.jpg']
+}
+
+
+# leader_score = max(CATS_DB.values())[0]
+# print(leader_score)
